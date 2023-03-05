151fab0a825d9b528318f6e3f59fea74195fa739
EfSoren <ef.sorensen@me.com>
2023-03-04 17:29:47 -0700

    apollo functionality

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 import { BrowserRouter } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3001/graphql",
+  cache: new InMemoryCache(),
+});
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ApolloProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById("root")
 );
