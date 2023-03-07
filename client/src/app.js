ebd79ec2d3e3edbcc965c6a12ea71a41a5ebbc8b
EfSoren <ef.sorensen@me.com>
2023-03-07 15:43:01 -0700

    merge errors

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/* import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from "./components/Layout";
 import Nav from "./components/Nav";
@@ -45,6 +45,57 @@ function App() {
         </Switch>
       </Layout>
     </Router>
+  );
+}
+
+export default App;
+ */
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import Nav from "./components/nav";
+import Cards from "./components/cards";
+import Current from "./components/current_card";
+import LandingPage from "./components/landing_page";
+import Dashboard from "./components/dashboard";
+import Create from "./components/create_project";
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  /* const renderPage = () => {
+    if (currentPage === "All Projects") {
+      return (
+        <Cards currentPage={currentPage} handlePageChange={handlePageChange} />
+      );
+    }
+    if (currentPage === "Single Project") {
+      return <Current />;
+    }
+  }; */
+
+  /*  return (
+    <>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </>
+  );
+  */
+  const userId = "6405442e968973138d97f8e5";
+  return (
+    <Routes>
+      <Route path="/home" element={<Nav />}>
+        <Route index element={<Cards />} />
+        <Route path="single" element={<Current />} />
+        <Route path={`${userId}`} element={<Cards />} />
+        <Route path=":test" element={<Current />} />
+        <Route path="create" element={<Create />} />
+      </Route>
+    </Routes>
   );
 }
 
