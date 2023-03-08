b7d3d96446367a5cc893bacc4a9a0cdba7a0ea4f
EfSoren <ef.sorensen@me.com>
2023-03-07 19:02:06 -0700

    correct browser routing

--- client/src/index.js
+++ client/src/index.js
@@ -2,23 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 import { BrowserRouter } from "react-router-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  gql,
-  ApolloProvider,
-} from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: "http://localhost:3001/graphql",
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ApolloProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
   document.getElementById("root")
 );
