e0faa60c8d38c18605af89dd6ec41de2442458a8
mgaskins17 <mattgaskins77@gmail.com>
2023-03-09 16:40:51 -0700

    fixed date format for projects

--- client/src/components/current_card.js
+++ client/src/components/current_card.js
@@ -14,16 +14,27 @@ function Current() {
   }
 
   const project = data?.getProject || {};
-  const createTimestamp = new Date().getTime(project.createdAt);
-  const createdTime = new Date(createTimestamp);
-  const endTimestamp = new Date().getTime(project.endDate);
-  const endTime = new Date(endTimestamp);
-  console.log(
-    `${createdTime.getDate()}/${
-      createdTime.getMonth() + 1
-    }/${createdTime.getFullYear()}`
-  );
-  console.log(project);
+  const createdAtTime = Number(project.createdAt);
+  const enddateAtTime = Number(project.endDate);
+  console.log(createdAtTime, enddateAtTime);
+  const convertCreated = new Date(createdAtTime);
+  const convertEndDate = new Date(enddateAtTime);
+  console.log(convertCreated, convertEndDate);
+  console.log(convertCreated.toLocaleDateString("en-US"));
+  console.log(convertEndDate.toLocaleDateString("en-US"));
+  const localCreate = convertCreated.toLocaleDateString("en-US");
+  const localEnd = convertEndDate.toLocaleDateString("en-US");
+
+  // const createTimestamp = new Date().getTime(project.createdAt);
+  // const createdTime = new Date(createTimestamp);
+  // const endTimestamp = new Date().getTime(project.endDate);
+  // const endTime = new Date(endTimestamp);
+  // console.log(
+  //   `${createdTime.getDate()}/${
+  //     createdTime.getMonth() + 1
+  //   }/${createdTime.getFullYear()}`
+  // );
+  // console.log(project);
 
   const checkDes = () => {
     if (!project.description) {
@@ -39,12 +50,14 @@ function Current() {
       <h1 className="single-project-title">{project.projectName}</h1>
       {checkDes()}
       <p>
-        Created on: {createdTime.getMonth() + 1}/{createdTime.getDate()}/
-        {createdTime.getFullYear()}{" "}
+        Created on: {localCreate}
+        {/* {createdTime.getMonth() + 1}/{createdTime.getDate()}/
+        {createdTime.getFullYear()}{" "} */}
       </p>
       <p>
-        Project Deadline: {endTime.getMonth() + 1}/{endTime.getDate()}/
-        {endTime.getFullYear()}{" "}
+        Project Deadline: {localEnd}
+        {/* {endTime.getMonth() + 1}/{endTime.getDate()}/
+        {endTime.getFullYear()}{" "} */}
       </p>
       <p>{project._id}</p>
       {/* Display other project details here */}
