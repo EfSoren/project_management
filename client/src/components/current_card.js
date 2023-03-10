bfadbaca6c528f3536d7c6ca593ab506eed133f2
mgaskins17 <mattgaskins77@gmail.com>
2023-03-09 18:46:51 -0700

    added tasks and team members

--- client/src/components/current_card.js
+++ client/src/components/current_card.js
@@ -7,21 +7,16 @@ function Current() {
   const { loading, data } = useQuery(QUERY_PROJECT, {
     variables: { projectId },
   });
-
   if (loading) {
     return <div>Loading...</div>;
   }
-
   const project = data?.getProject || {};
   const createdAtTime = Number(project.createdAt);
   const enddateAtTime = Number(project.endDate);
-
   const convertCreated = new Date(createdAtTime);
   const convertEndDate = new Date(enddateAtTime);
- 
   const localCreate = convertCreated.toLocaleDateString("en-US");
   const localEnd = convertEndDate.toLocaleDateString("en-US");
-
   const checkDes = () => {
     if (!project.description) {
       const projDescription = "No description provided";
@@ -31,13 +26,11 @@ function Current() {
       return <h3>{projDescription}</h3>;
     }
   };
-
   console.log(project.teams[0].users[0]);
   const teamList = project.teams[0].users;
   function Team({ firstname, lastname }) {
     return (
       <li>
-        {" "}
         {firstname} {lastname}
       </li>
     );
@@ -46,15 +39,17 @@ function Current() {
     <section className="single-project-wrapper">
       <h1 className="single-project-title">{project.projectName}</h1>
       {checkDes()}
-      <p>
-        Created on: {localCreate}
-      </p>
-      <p>
-        Project Deadline: {localEnd}
-      </p>
+      <ul>No Current Tasks Assigned</ul>
+      <ul>
+        Team Members
+        {teamList.map((index) => (
+          <Team key={index.firstname} {...index} />
+        ))}
+      </ul>
+      <p>Created on: {localCreate}</p>
+      <p>Project Deadline: {localEnd}</p>
       <p>{project._id}</p>
     </section>
   );
 }
-
 export default Current;
